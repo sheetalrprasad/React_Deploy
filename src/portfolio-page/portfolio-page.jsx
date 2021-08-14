ff67cafa30d7260870ee9383af4a8af1aaaeeb00
sheetalrprasad <sheetalraniprasad@gmail.com>
2021-08-14 17:37:36 +0530

    my changes 1

---
+++ src/portfolio-page/portfolio-page.jsx
@@ -0,0 +1,29 @@
+// import { NavigationBar } from '../navigation';
+// import { Home } from '../home';
+// import { AboutMe } from '../about-me';
+// import { Skills } from '../skills';
+// import { MyProjects } from '../my-projects';
+// import { ContactMe } from '../contact-me';
+import './portfolio-page.scss';
+
+const PortfolioPage = () => {
+    return (
+        <div>
+            {/* <NavigationBar/> */}
+            <p className="underConstruction">Under Construction...</p>
+            {/* <div className="wrapper parent">
+                <div id="#home"><Home /></div> 
+                    <div className="scroll-bg bg-1 child">
+                        <div id="about"><AboutMe /> </div>
+                        <div className="bg-2">
+                            <div id="skills"> <Skills /> </div>
+                            <div id="projects" > <MyProjects /></div>
+                            <div id="contact" > <ContactMe /></div>
+                        </div>
+                    </div>
+                </div> */}
+            </div>
+    );
+};
+
+export default PortfolioPage;
